cb4aea46258b2743de47fc333178b160e52af4c6
hydrocarbo <43991258+hydrocarbo@users.noreply.github.com>
2019-07-04 18:56:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var data = [{
       description: 'Airplane',
       objAsset: 'airplane_asset_obj',
       mtlAsset: 'airplane_asset_mtl',
-      scale: '0.3 0.3 0.3',
+      scale: '0.1 0.1 0.1',
       scaleOne: '6 6 6',
       scaleTwo: '8 8 8',
       isObj: true
